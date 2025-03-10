d03549c0599ad9ead3c9b0f1ccf8c6e0580739c0
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-03-10 18:29:45 +0700

    build socket.io into app.js to task socket from frontend

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import express from "express";
+
 import taskRouter from "./routes/task.js";
 import cors from "cors";
+import http from "http";
+
+import { Server } from "socket.io";
 
 const app = express();
-
-app.use(express.json());
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000", // Domain that can access server
+        method: ["POST", "GET", "PUT", "DELETE"], // Set Methods that allowed
+        allowedHeaders: ["Content-Type", "Authorization"], // headers that allowed
+        credentials: true, // Set access for cookie and headers
+    },
+});
 
 app.use(
     cors({
@@ -15,8 +26,18 @@ app.use(
     })
 );
 
+app.use(express.json());
+
 app.use("/api", taskRouter);
 
-app.listen(3001, () => {
+io.on("connection", (socket) => {
+    console.log("a user connect");
+
+    socket.on("disconnect", () => {
+        console.log("a user disconnect");
+    });
+});
+
+server.listen(3001, () => {
     console.log("Server running at Port 3000");
 });
